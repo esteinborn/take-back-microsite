22859b255c0d9f97b0b4572aedcf3ff296be154b
Chandra Galbiati <chandra.galbiati@gmail.com>
2018-04-17 00:14:50 -0400

    Firebase api interaction to add reminders

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 const HOST = '0.0.0.0';
+const firebaseApi = require('./firebase');
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '../build')));
@@ -11,6 +12,15 @@ app.use(express.static(path.join(__dirname, '../build')));
 // API routes go here
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
+});
+
+app.post('/api/add_reminder', (req, res) => {
+  try {
+    firebaseApi.handleAddReminder(req.body);
+    res.status(200).send('ok');
+  } catch (e) {
+    res.status(500).send('Error adding reminder');
+  }
 });
 
 app.get('*', (req, res) => {
