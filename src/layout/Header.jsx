f5c24b9153ea803c5d0c910b2dba7d148f51a869
Meka <mekaseymour@gmail.com>
2018-04-18 09:31:55 -0400

    add rotating title (#25)

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -10,7 +10,19 @@ import heroImage from "../assets/hero-image.png";
 
 const Header = () => (
   <SectionContainerGray className="top-section">
-    <SectionTitle>{headerCopy.title}</SectionTitle>
+    <SectionTitle>
+      <div className="rotating-title">
+        <p className="title">Take Back Day.</p>
+        <b className="rotating-section">
+          <span className="rotating-section-inner">
+            Stop addiction.<br />
+            Protect our waters.<br />
+            End the opiod crisis.<br />
+          </span>
+        </b>
+      </div>
+    </SectionTitle>
+    <div className="sub-title">on April 28th</div>
     <Image src={heroImage} />
     <SectionDescription>{headerCopy.description}</SectionDescription>
   </SectionContainerGray>
